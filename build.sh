6828fbe000f7943e9d6edade9d0d663e976b5eec
budblack <budblack@qq.com>
2018-11-24 22:33:57 +0800

    # add keystore.

--- build.sh
+++ build.sh
@@ -1,3 +1,17 @@
+##############################
+# cd ./sub/spa
+# yarn
+# yarn build
+# 
+# cd ../..
+# rm -rf ./www/*
+# mv ./sub/spa/dist/* ./www/
+##############################
+sudo chmod -R ugo+rw $ANDROID_HOME
+cordova platforms rm android 
+cordova platforms add android
 cordova clean
-cordova platform rm android && cordova platform add android
-cordova build android -- --gradleArg=-PcdvBuildMultipleApks=false
+cordova prepare
+cordova build android -release -- --gradleArg=-PcdvBuildMultipleApks=false
+jarsigner -verbose -keystore ./net.msign.tool.util.keystore -signedjar $(pwd)/platforms/android/app/build/outputs/apk/release/app-release-signed.apk $(pwd)/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk net.msign.tool.util.keystore -storepass budblack -keypass budblack
+rm $(pwd)/platforms/android/app/build/outputs/apk/release/app-release-unsigned.apk 
